491f48c754b632f65436d5265ac93080e63eb367
MihirKapile <mihirkapile@gmail.com>
2023-08-07 16:36:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
+import "dotenv/config";
+
+const app = express();
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL
+}));
+    const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+//app.get('/hello', (req, res) => {res.send('Life is good!')})
+//app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(express.json());
+TuitsController(app);
+HelloController(app)
+UserController(app)
+app.listen(process.env.PORT || 4000);
+AuthController(app);
